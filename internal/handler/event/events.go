b34ea3b633a082bc05cfc92ce943ba47444a1dba
Mehdi <mehditeymorian322@gmail.com>
2021-10-15 12:04:15 +0330

    FEAT: add HTTP handlers

--- internal/handler/event/events.go
+++ internal/handler/event/events.go
@@ -1,23 +1,105 @@
-package event
+package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/mehditeymorian/etefagh/internal/model"
+	store "github.com/mehditeymorian/etefagh/internal/store/event"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"net/http"
+)
 
-func RetrieveAll(c echo.Context) error {
-
-	return nil
+// Event events handler struct
+// provide database connection
+type Event struct {
+	Store store.Event
 }
 
-func Retrieve(c echo.Context) error {
+// RetrieveAll events
+func (e Event) RetrieveAll(c echo.Context) error {
 
-	return nil
+	// retrieve all events
+	// WARNING!!!!!! require limitation
+	all, err := e.Store.RetrieveAll(c.Request().Context())
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	// set result to an empty array if no event is presents
+	if all == nil {
+		all = []model.Event{}
+	}
+
+	return c.JSON(http.StatusOK, all)
 }
 
-func Create(c echo.Context) error {
+// Retrieve an event by id
+func (e Event) Retrieve(c echo.Context) error {
+	eventId := c.Param("event_id")
 
-	return nil
+	retrieve, err := e.Store.Retrieve(c.Request().Context(), eventId)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	// return not found if no event founded
+	if retrieve == nil {
+		return c.JSON(http.StatusNotFound, "")
+	}
+
+	return c.JSON(http.StatusOK, retrieve)
 }
 
-func Delete(c echo.Context) error {
+// Create an event
+func (e Event) Create(c echo.Context) error {
 
-	return nil
+	var event model.Event
+	// read body
+	if err := c.Bind(&event); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	// recreate event to avoid passing fields that should not be bindable
+	event = model.Event{
+		EventType:   event.EventType,
+		Description: event.Description,
+		Priority:    event.Priority,
+		Payload:     event.Payload,
+		CreatedAt:   0,
+		Id:          primitive.ObjectID{},
+	}
+
+	// validate input
+	if err := event.Validate(); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	// create event
+	id, err := e.Store.Create(c.Request().Context(), event)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	// publish event
+
+	return c.JSON(http.StatusOK, id)
+}
+
+// Delete an event
+func (e Event) Delete(c echo.Context) error {
+	eventId := c.Param("event_id")
+
+	// delete event
+	if err := e.Store.Delete(c.Request().Context(), eventId); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusNoContent, "")
+}
+
+// Register register events endpoints on the HTTP server
+func (e Event) Register(group *echo.Group) {
+	group.GET("/events", e.RetrieveAll)
+	group.GET("/events/:event_id", e.Retrieve)
+	group.POST("/events", e.Create)
+	group.DELETE("/events/:event_id", e.Delete)
 }
