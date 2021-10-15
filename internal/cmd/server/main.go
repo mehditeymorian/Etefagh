3739ba186533e86ed34c66d0e4ba02deb7be0520
Mehdi <mehditeymorian322@gmail.com>
2021-10-15 12:05:46 +0330

    FEAT: add server start point

---
+++ internal/cmd/server/main.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"errors"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/mehditeymorian/etefagh/internal/config"
+	"github.com/mehditeymorian/etefagh/internal/db"
+	handler "github.com/mehditeymorian/etefagh/internal/handler/event"
+	store "github.com/mehditeymorian/etefagh/internal/store/event"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Main(config config.Config) {
+
+	// create HTTP server
+	app := echo.New()
+
+	// create a database connection
+	database, err := db.Connect(config.DB)
+	if err != nil {
+		panic(fmt.Sprintf("database initialization failed %w", err))
+	}
+
+	// register events endpoints
+	handler.Event{Store: store.NewMongoEvent(database)}.Register(app.Group("/api/v1"))
+
+	// start HTTP Server
+	if err := app.Start(fmt.Sprintf(":%s", config.Api.Port)); !errors.Is(err, http.ErrServerClosed) {
+		panic("echo initiation failed")
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+}
