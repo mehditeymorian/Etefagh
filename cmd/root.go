d3dbcac0d6c371fd15d1db212824f823e37694c6
Mehdi <mehditeymorian322@gmail.com>
2021-10-16 09:50:06 +0330

    FIX: setup configuration to start the server

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/mehditeymorian/etefagh/internal/cmd"
+import (
+	"github.com/mehditeymorian/etefagh/internal/cmd/server"
+	"github.com/mehditeymorian/etefagh/internal/config"
+	"github.com/mehditeymorian/etefagh/internal/db"
+	"github.com/mehditeymorian/etefagh/internal/handler"
+)
 
 func main() {
-	cmd.Start()
+	var config config.Config = config.Config{
+		Api: handler.Config{
+			Port: "3000",
+		},
+		DB: db.Config{
+			Uri:  "mongodb://localhost:27017",
+			Name: "events",
+		},
+	}
+	server.Main(config)
 }
