d3457d7f368b70872543db7b55383dadb9b914d3
Mehdi <mehditeymorian322@gmail.com>
2021-10-18 18:26:34 +0330

    FEAT: add ackId to event model && json tag to fields

--- internal/model/event.go
+++ internal/model/event.go
@@ -6,10 +6,11 @@ import (
 
 // Event model
 type Event struct {
-	EventType   string             `bson:"event_type"`
-	Description string             `bson:"description,omitempty"`
-	Priority    int                `bson:"priority"`
-	Payload     string             `bson:"payload,omitempty"`
-	CreatedAt   primitive.DateTime `bson:"created_at"`
-	Id          primitive.ObjectID `bson:"_id"`
+	EventType   string             `bson:"event_type"  json:"event_type,omitempty"`
+	Description string             `bson:"description,omitempty" json:"description,omitempty"`
+	Priority    int                `bson:"priority" json:"priority,omitempty"`
+	Payload     string             `bson:"payload,omitempty" json:"payload,omitempty"`
+	AckId       string             `bson:"ack_id,omitempty" json:"ack_id,omitempty"`
+	CreatedAt   primitive.DateTime `bson:"created_at" json:"created_at,omitempty"`
+	Id          primitive.ObjectID `bson:"_id" json:"id,omitempty"`
 }
